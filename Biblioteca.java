d6c3f4e3d7888fcb48404f3d759eeef672b179ae
scaua1675-ai <scaua1675@gmail.com>
2026-06-10 23:20:26 -0300

    Create Biblioteca class to manage books

---
+++ Biblioteca.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Biblioteca {
+    private String nome;
+    private List<Livro> livros = new ArrayList<>();
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Livro> getLivros() {
+        return livros;
+    }
+
+    public void adicionarLivro(Livro livro) {
+        livros.add(livro);
+    }
+}
