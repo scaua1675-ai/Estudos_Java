c9cc9c0426e0692cb9b7d04a22a6778cf0905bea
scaua1675-ai <scaua1675@gmail.com>
2026-06-10 23:16:32 -0300

    Add Capitulo class with title property and methods

---
+++ Capitulo.java
@@ -0,0 +1,11 @@
+public class Capitulo {
+    private String titulo;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+}
